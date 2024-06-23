2d8726a2c7d509ad2bab780df84cf0d0f1edcc6b
itamarTrainers <itamartrainers1@gmail.com>
2024-06-23 12:42:25 +0300

    comm

--- CommunicationManager.h
+++ CommunicationManager.h
@@ -3,25 +3,24 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/bind/bind.hpp>
 #include <thread>
 #include <mutex>
 #include <deque>
-#include <string> // Include the string header
+#include <string>
+#include <atomic>
 
 class CommunicationManager {
 public:
     CommunicationManager(boost::asio::io_service& io_service, const std::string& port, unsigned int baud_rate);
+    ~CommunicationManager();
 
     void start();
     void write(const std::string& data);
     void close();
 
 private:
-    void read();
-    void handle_read(const boost::system::error_code& ec, std::size_t bytes_transferred);
+    void read_loop();
     void do_write(const std::string& data);
-    void write();
     void handle_write(const boost::system::error_code& ec, std::size_t bytes_transferred);
     void do_close();
 
@@ -29,6 +28,9 @@ private:
     boost::asio::serial_port serial_port_;
     std::deque<std::string> write_msgs_;
     std::string read_msg_;
+    std::thread read_thread_;
+    std::atomic<bool> run_read_thread_;
+    std::mutex write_mutex_;
 };
 
 #endif // COMMUNICATION_MANAGER_H
