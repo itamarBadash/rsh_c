a5ebb3a2fc0632b473b882d6852b535f9c1c8cfa
itamarTrainers <itamartrainers1@gmail.com>
2024-06-30 13:17:53 +0300

    comm

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <mavsdk/plugins/telemetry/telemetry.h>
 #include <iostream>
 #include <thread>
-#include <atomic>
 #include "TelemetryManager.h"
 #include "CommandManager.h"
 #include "CommunicationManager.h"
 
-int commandManagerTest(std::shared_ptr<mavsdk::System> system);
-int TelemetryManagerTest(std::shared_ptr<System> system, std::shared_ptr<CommunicationManager> comm);
+
+void main_thread_function(std::shared_ptr<System> system, std::shared_ptr<CommandManager> command_manager,std::shared_ptr<CommunicationManager> communications_manager, std::shared_ptr<TelemetryManager> telemetry_manager);
 
 using namespace mavsdk;
 
@@ -21,91 +20,6 @@ void usage(const std::string& bin_name) {
               << "For example, to connect to the simulator use URL: udp://:14540\n";
 }
 
-int TelemetryManagerTest(std::shared_ptr<System> system, std::shared_ptr<CommunicationManager> comm) {
-    try {
-        TelemetryManager telemetry_manager(system,comm);
-        telemetry_manager.start();
-
-        while (telemetry_manager.isRunning()) {
-            std::this_thread::sleep_for(std::chrono::seconds(5));
-        }
-
-        telemetry_manager.stop();
-    } catch (const std::runtime_error& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
-    return 0;
-}
-
-int commandManagerTest(std::shared_ptr<System> system) {
-    CommandManager command_manager(system);
-
-    // Test arming the system
-    CommandManager::Result result = command_manager.arm();
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Arm failed\n";
-        return 1;
-    } else {
-        std::cout << "Arm successful\n";
-    }
-
-    // Test taking off
-    result = command_manager.takeoff();
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Takeoff failed\n";
-        return 1;
-    } else {
-        std::cout << "Takeoff successful\n";
-    }
-
-    // Test landing
-    result = command_manager.land();
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Land failed\n";
-        return 1;
-    } else {
-        std::cout << "Land successful\n";
-    }
-
-    // Test returning to launch
-    result = command_manager.return_to_launch();
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Return to launch failed\n";
-        return 1;
-    } else {
-        std::cout << "Return to launch successful\n";
-    }
-
-    // Test setting flight mode
-    result = command_manager.set_flight_mode(1, 2);
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Set flight mode failed\n";
-        return 1;
-    } else {
-        std::cout << "Set flight mode successful\n";
-    }
-
-    // Test setting manual control
-    result = command_manager.set_manual_control(0.0, 0.0, 0.5, 0.0);
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Set manual control failed\n";
-        return 1;
-    } else {
-        std::cout << "Set manual control successful\n";
-    }
-
-    // Test disarming the system
-    result = command_manager.disarm();
-    if (result != CommandManager::Result::Success) {
-        std::cerr << "Disarm failed\n";
-        return 1;
-    } else {
-        std::cout << "Disarm successful\n";
-    }
-
-    return 0;
-}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -147,12 +61,19 @@ int main(int argc, char** argv) {
 
 
     auto command_manager = std::make_shared<CommandManager>(system);
-
     auto communication_manager = std::make_shared<CommunicationManager>("/dev/ttyUSB0",57600, command_manager);
+    auto telemetry_manager = std::make_shared<TelemetryManager>(system,communication_manager);
 
-    std::thread telemetry_thread(TelemetryManagerTest, system, communication_manager);
+    std::thread main_thread(main_thread, system, command_manager,communication_manager,telemetry_manager);
 
     telemetry_thread.join();
 
     return 0;
+}
+
+void main_thread_function(std::shared_ptr<System> system, std::shared_ptr<CommandManager> command_manager,std::shared_ptr<CommunicationManager> communications_manager, std::shared_ptr<TelemetryManager> telemetry_manager){
+    while(true){
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        //logic for error handling and exeptions or retry connections with modules.
+    }
 }
