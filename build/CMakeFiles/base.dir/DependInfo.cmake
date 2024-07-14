f2aa6ee259e5da1d48b71a185d6fd1841ca76992
itamar <itamar@ardusim.myguest.virtualbox.org>
2024-07-14 16:03:52 +0300

    config

---
+++ build/CMakeFiles/base.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/itamar/rsh_c/CommandManager.cpp" "CMakeFiles/base.dir/CommandManager.cpp.o" "gcc" "CMakeFiles/base.dir/CommandManager.cpp.o.d"
+  "/home/itamar/rsh_c/CommunicationManager.cpp" "CMakeFiles/base.dir/CommunicationManager.cpp.o" "gcc" "CMakeFiles/base.dir/CommunicationManager.cpp.o.d"
+  "/home/itamar/rsh_c/TelemetryManager.cpp" "CMakeFiles/base.dir/TelemetryManager.cpp.o" "gcc" "CMakeFiles/base.dir/TelemetryManager.cpp.o.d"
+  "/home/itamar/rsh_c/main.cpp" "CMakeFiles/base.dir/main.cpp.o" "gcc" "CMakeFiles/base.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/base.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base.dir/CommandManager.cpp.o"
+  "CMakeFiles/base.dir/CommandManager.cpp.o.d"
+  "CMakeFiles/base.dir/CommunicationManager.cpp.o"
+  "CMakeFiles/base.dir/CommunicationManager.cpp.o.d"
+  "CMakeFiles/base.dir/TelemetryManager.cpp.o"
+  "CMakeFiles/base.dir/TelemetryManager.cpp.o.d"
+  "CMakeFiles/base.dir/main.cpp.o"
+  "CMakeFiles/base.dir/main.cpp.o.d"
+  "base"
+  "base.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
