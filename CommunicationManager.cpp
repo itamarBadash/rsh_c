d22d51d95403ed69b879c08772fdfd4925fec838
itamarTrainers <itamartrainers1@gmail.com>
2024-07-15 15:06:34 +0300

    config test

--- CommunicationManager.cpp
+++ CommunicationManager.cpp
@@ -10,6 +10,8 @@
 #include <string>
 #include <functional>
 #include <map>
+#include <unordered_map>
+#include <stdexcept>
 
 CommunicationManager::CommunicationManager(const std::string &port, int baud_rate, std::shared_ptr<CommandManager> cmd_manager)
         : port_name(port), baud_rate(baud_rate), serial_port(-1), stop_flag(false), command_manager(cmd_manager) {
@@ -20,6 +22,44 @@ CommunicationManager::CommunicationManager(const std::string &port, int baud_rat
 CommunicationManager::~CommunicationManager() {
     stopWorker();
     closePort();
+}
+
+speed_t convertBaudRate(int baudRate) {
+    switch (baudRate) {
+        case 0: return B0;
+        case 50: return B50;
+        case 75: return B75;
+        case 110: return B110;
+        case 134: return B134;
+        case 150: return B150;
+        case 200: return B200;
+        case 300: return B300;
+        case 600: return B600;
+        case 1200: return B1200;
+        case 1800: return B1800;
+        case 2400: return B2400;
+        case 4800: return B4800;
+        case 9600: return B9600;
+        case 19200: return B19200;
+        case 38400: return B38400;
+        case 57600: return B57600;
+        case 115200: return B115200;
+        case 230400: return B230400;
+        case 460800: return B460800;
+        case 500000: return B500000;
+        case 576000: return B576000;
+        case 921600: return B921600;
+        case 1000000: return B1000000;
+        case 1152000: return B1152000;
+        case 1500000: return B1500000;
+        case 2000000: return B2000000;
+        case 2500000: return B2500000;
+        case 3000000: return B3000000;
+        case 3500000: return B3500000;
+        case 4000000: return B4000000;
+        default:
+            throw std::invalid_argument("Unsupported baud rate: " + std::to_string(baudRate));
+    }
 }
 
 void CommunicationManager::openPort() {
@@ -39,8 +79,8 @@ void CommunicationManager::openPort() {
         return;
     }
 
-    cfsetospeed(&tty, B57600); // Set output speed
-    cfsetispeed(&tty, B57600); // Set input speed
+    cfsetospeed(&tty, convertBaudRate(baud_rate)); // Set output speed
+    cfsetispeed(&tty, convertBaudRate(baud_rate)); // Set input speed
 
     tty.c_cflag &= ~PARENB; // No parity bit
     tty.c_cflag &= ~CSTOPB; // Only one stop bit
