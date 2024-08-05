089e8ceeed76c090bc79bb491b83b046a50fc4f9
itamarTrainers <itamartrainers1@gmail.com>
2024-08-05 17:25:18 +0300

    TCP

--- Src/Communications/TCPServer.cpp
+++ Src/Communications/TCPServer.cpp
@@ -84,7 +84,6 @@ void TCPServer::handleClient(int clientSocket) {
         } else if (bytesReceived == 0) {
             std::cout << "Client disconnected." << std::endl;
             break;
-            std::cout << "Received: " << std::endl;
         }
 
         buffer[bytesReceived] = '\0'; buffer << std::endl;
