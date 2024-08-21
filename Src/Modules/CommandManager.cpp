fb6a3075c926f5f437ff8171a84175c83930e42e
itamarTrainers <itamartrainers1@gmail.com>
2024-08-21 14:27:36 +0300

    added manual control function ( for easier use params are pitch yaw throttle roll) also tests for rc refactor

--- Src/Modules/CommandManager.cpp
+++ Src/Modules/CommandManager.cpp
@@ -38,7 +38,7 @@ void CommandManager::initialize_command_handlers() {
             }},
             {"set_manual_control", [this](const std::vector<float>& params) {
                 if (params.size() == 4) {
-                    return set_manual_control(params[0], params[1], params[2], params[3]);
+                    return send_manual_control(params[0], params[1], params[2], params[3]);
                 }
                 return Result::Failure;
             }},
